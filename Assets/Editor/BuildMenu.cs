b988943d843854b830ee1d007501aab8d3478e0f
talyh <taly.hotimsky@gmail.com>
2017-10-22 11:24:25 -0400

    expanded build functionality to target all needed platforms

--- Assets/Editor/BuildMenu.cs
+++ Assets/Editor/BuildMenu.cs
@@ -5,23 +5,71 @@ using UnityEditor;
 
 public class BuildMenu
 {
+    // Build for iOS 
     [MenuItem("Build/iOS/Dev")]
     public static void BuildIOSDev()
     {
         BuildIOS(true);
     }
 
+    [MenuItem("Build/iOS/Rel")]
+    public static void BuildIOSRel()
+    {
+        BuildIOS();
+    }
+
+    [MenuItem("Build/iOS/All")]
+    public static void BuildIOSAll()
+    {
+        BuildIOS();
+        BuildIOS(true);
+    }
+
     static void BuildIOS(bool devBuild = false)
     {
         Build(BuildTarget.iOS, devBuild);
     }
+
+    // Build for Android
+    [MenuItem("Build/Android/Dev")]
+    static void BuildAndroidDev()
+    {
+        BuildAndroid(true);
+    }
+
+    [MenuItem("Build/Android/Rel")]
+    static void BuildAndroidRel()
+    {
+        BuildAndroid();
+    }
+
+    [MenuItem("Build/Android/All")]
+    public static void BuildAndroidAll()
+    {
+        BuildAndroid();
+        BuildAndroid(true);
+    }
+
+    static void BuildAndroid(bool devBuild = false)
+    {
+        Build(BuildTarget.Android, devBuild);
+    }
+
+
+    // Build for All Target Platforms
+    [MenuItem("Build/All")]
+    public static void BuildAll()
+    {
+        BuildIOSAll();
+        BuildAndroidAll();
+    }
 
     static void Build(BuildTarget target, bool devBuild = false)
     {
         // determine relative path to help determine where to build to
         string pathToAssets = Application.dataPath;
         string pathToProject = pathToAssets.Replace("/Assets", "");
-        string buildPath = string.Format("{0}/Builds/{1}{2}/InfiniteRunner{3}", pathToProject, target, devBuild ? "Dev" : "Rel", 1); // TODO append release control
+        string buildPath = string.Format("{0}/Builds/{1}/{2}/InfiniteRunner{3}", pathToProject, target, devBuild ? "Dev" : "Rel", 1); // TODO append release control
 
         // configure the options for the build
         BuildPlayerOptions options = new BuildPlayerOptions();
