8adde7518d3ac96569198fe5168e90f72e46c246
talyh <taly.hotimsky@gmail.com>
2017-11-06 11:48:01 -0500

    Localization loading implemented

---
+++ Assets/CanvasController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Localization.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Localization.cs
+++ Assets/Scripts/Localization.cs
@@ -2,18 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Localization : MonoBehaviour
+public static class Localization
 {
+    private const string ENGLISH_KEY = "ENGLISH";
+    private const string FRENCH_KEY = "FRENCH";
+    private const string PORTUGUESE_KEY = "PORTUGUESE";
 
-    // Use this for initialization
-    void Start()
+    static Dictionary<string, string> localizations;
+
+    public static void Initialize()
     {
+        // initialize Dictionary here to be sure it's initialized when we need it to
+        localizations = new Dictionary<string, string>();
 
+        //Load the CSV
+        TextAsset csv = Resources.Load<TextAsset>("Localizations");
+
+        //Get the system language
+        SystemLanguage language = Application.systemLanguage;
+
+        //Default language to english
+        int currentLanguageIndex = -1;
+
+        // break the csv into rows
+        string[] rows = csv.text.Split('\n');
+
+        // use the first row headers, starting in column 2, to identify the languages
+        string[] languageIndex = rows[0].Split(',');
+
+        for (int i = 1; i < languageIndex.Length; i++)
+        {
+            //Get the language index
+            if (language.ToString().ToUpper() == languageIndex[i].Trim().ToUpper())
+            {
+                currentLanguageIndex = i;
+            }
+        }
+
+        for (int i = 1; i < rows.Length; i++)
+        {
+            //Parse the csv into the dictionary
+            string[] columns = rows[i].Split(',');
+
+            string key = columns[0].Trim();
+            string value = columns[currentLanguageIndex].Trim();
+
+            localizations.Add(key, value);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // //Get the localized string
+    // public static string Get(string key)
+    // {
+    //     //Make sure the CSV has been loaded
+    //     if ( == null)
 
-    }
+
+    //         return
+
+    // }
+
+    // //A fancy way to get the localization string, not necessary 
+    // public static string Localize(this string key)
+    // {
+
+    // }
 }
