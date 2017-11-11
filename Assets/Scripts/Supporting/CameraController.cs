ff52a793c313e6bb0b0b203ce6854807a0deff43
talyh <taly.hotimsky@gmail.com>
2017-11-10 19:55:52 -0500

    Adjusted Camera to work with different resolutions

--- Assets/Scripts/Supporting/CameraController.cs
+++ Assets/Scripts/Supporting/CameraController.cs
@@ -4,10 +4,61 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private const float INITIAL_X_FOR_1_3 = 5.4f;
+    private const float INITIAL_X_FOR_1_6 = 6.8f;
+    private const float INITIAL_X_FOR_1_7 = 7.6f;
+
     private Transform _target;
     private float _offset;
 
+    private float _resolution;
+
+
+    private void Awake()
+    {
+        DetermineResolution();
+        AdjustInitialPositionBasedOnResolution();
+    }
+
     private void Start()
+    {
+        RunInitialChecks();
+    }
+
+    void LateUpdate()
+    {
+        // follow the player, keeping a stable distance
+        if (_target)
+        {
+            transform.position = new Vector3(_target.position.x - _offset, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void DetermineResolution()
+    {
+        Camera cam = GetComponent<Camera>();
+        float width = cam.scaledPixelWidth;
+        float height = cam.scaledPixelHeight;
+        _resolution = width / height;
+    }
+
+    private void AdjustInitialPositionBasedOnResolution()
+    {
+        if (_resolution <= 1.4f)
+        {
+            transform.position = new Vector3(INITIAL_X_FOR_1_3, transform.position.y, transform.position.z);
+        }
+        else if (_resolution >= 1.5f && _resolution <= 1.7f)
+        {
+            transform.position = new Vector3(INITIAL_X_FOR_1_6, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(INITIAL_X_FOR_1_7, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void RunInitialChecks()
     {
         // Find the player
         _target = GameController.instance.character;
@@ -15,22 +66,6 @@ public class CameraController : MonoBehaviour
         if (Supporting.CheckRequiredProperty(gameObject, _target, "Target"))
         {
             _offset = _target.position.x - transform.position.x;
-        }
-    }
-
-    void LateUpdate()
-    {
-        // if the player is still alive, move the camera to the player's position, clamping it to the level edges
-        if (_target)
-        {
-            // Vector3 newCameraPosition = new Vector3(
-            //    Mathf.Clamp(_target.position.x, leftEdge, rightEdge),
-            //    Mathf.Clamp(_target.position.y, bottomEdge, topEdge),
-            //    transform.position.z);
-            // using Lerp we have a smoother transition
-            // transform.position = Vector3.Lerp(transform.position, newCameraPosition, speed * Time.deltaTime);
-            // Vector3 newCameraPosition = new Vector3(_target.position.x - _offset, transform.position.y, transform.position.z);
-            transform.position = new Vector3(_target.position.x - _offset, transform.position.y, transform.position.z);
         }
     }
 }
